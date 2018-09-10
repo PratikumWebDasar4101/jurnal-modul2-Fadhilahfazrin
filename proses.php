728fa4bcb87a8ced78232f68ef90d00ef50a77d7
Fadhilahfazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-09-10 09:58:29 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -15,4 +15,21 @@
 			rekusif_kolom($batas, $awal);
 		}
 	}
+	function deret_ganjil_genap($value){
+		$arr_genap = array();
+		$arr_ganjil = array();
+		for ($i=1; $i <= value; $i++) { 
+			if ($i%2 == 0) 
+				array_push($arr_genap, $i);
+			else 
+				array_push($arr_ganjil, $i);
+		}
+		echo "Genap&emps; = ";
+		for ($i=0; $i < count($arr_genap) ; $i++) { 
+			echo $arr_genap[$i];
+			if ($i != count($arr_genap)-1) {
+				echo ", ";
+			}
+		}
+	}
 ?>
