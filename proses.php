5b23c545b79c5620a0181f7a8f3bd5d97cf76e6b
Fadhilahfazrin <42725270+Fadhilahfazrin@users.noreply.github.com>
2018-09-10 09:51:19 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,4 +1,18 @@
 <?php 
 //Tuliskan kode anda di bawah ini
-
+	function rekusif_baris($value){
+		rekusif_kolom($value);
+		echo "<br>";
+		$value--;
+		if ($value >= 1) {
+			rekusif_baris($value);
+		}
+	}
+	function rekusif_kolom($value){
+		echo "*";
+		$awal++;
+		if ($awal <= $batas) {
+			rekusif_kolom($batas, $awal);
+		}
+	}
 ?>
